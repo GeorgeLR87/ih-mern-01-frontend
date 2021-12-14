734f67e608deb763631c4166869872a8c56059de
GeorgeLR87 <jorgelrgarduno@gmail.com>
2021-12-14 16:55:11 -0600

    Contex y components

---
+++ src/context/Store/storeState.js
@@ -0,0 +1,47 @@
+
+import { useReducer } from 'react'
+import StoreContext from './StoreContext'
+
+import StoreReducer from './StoreReducer'
+
+const StoreState = (props) => {
+
+	// 1. INITIAL STATE (ESTADO INICIAL)
+	const initialState = {
+		stores: [],
+		hola: "mundo"
+	}
+
+	// 2. CONFIGURACIÓN DE REDUCER Y CREACIÓN DE ESTADO GLOBAL
+	const [globalState, dispatch] = useReducer(StoreReducer, initialState)
+
+	// 3. FUNCIONES DE CAMBIO EN ESTADO GLOBAL
+	const changeText = () => {
+
+		dispatch({ // ESTE OBJETO SE LE CONOCE COMO ACTION
+			type: "CHANGE_TEXT",
+			payload: "Estoy aprendiendo Context sin morir." 		
+		})
+
+	}
+
+
+
+	// 4. RETORNO
+	return (
+		<StoreContext.Provider
+			value={{
+				stores: globalState.stores,
+				hola: globalState.hola,
+				changeText
+
+			}}
+		>
+			{props.children}
+		</StoreContext.Provider>
+	)
+
+
+}
+
+export default StoreState
